d55fdc408f1fdd193715e74f2fa221f4895c7787
HyeWon <maru_05@naver.com>
2024-03-25 16:45:52 +0900

    Adding ListView
    1. Dynamically add an item in listview on button click

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+const String appTitle = 'Akatsuki';
 const Color losiColor = Color.fromARGB(255, 255, 36, 0);
 
 void main() {
@@ -14,14 +15,164 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Akatsuki',
+      title: appTitle,
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: losiColor),
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: ExampleParallax(),
-        )
-      )
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const Center(
+          child: MainPageBody(),
+        ),
+        bottomNavigationBar: const BottomAppBar(
+          child: Row(
+            children: [ CreateIconButton(), ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class MainPageBody extends StatefulWidget {
+  const MainPageBody({super.key});
+
+  @override State<MainPageBody> createState() =>
+      _MainPageBodyState();
+}
+
+class _MainPageBodyState extends State<MainPageBody> {
+  final myList = [
+    'hello',
+    'world'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(myList[index]),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return Divider();
+            },
+            itemCount: myList.length)
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: addItem,
+      ),
+    );
+  }
+
+  void addItem() {
+    setState(() {
+      myList.add('test');
+    });
+  }
+}
+
+class CreateIconButton extends StatelessWidget {
+  const CreateIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Center(
+        child: Ink(
+          decoration: const ShapeDecoration(
+            color: losiColor,
+            shape: CircleBorder(),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.create),
+            color: Colors.white,
+            onPressed: () {
+              showDialog(context: context, builder: (context){
+                return const CreateDialog();
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CreateDialog extends StatefulWidget {
+  const CreateDialog({super.key});
+
+  @override
+  State<CreateDialog> createState() =>
+      _CreateDialogState();
+}
+
+class _CreateDialogState extends State<CreateDialog> {
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    textController.addListener(() {
+      final String text = textController.text;
+      textController.value = textController.value.copyWith(
+        text: text,
+        selection:
+          TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: textController,
+              decoration: const InputDecoration(border: OutlineInputBorder()),
+              minLines: 10,
+              maxLines: 20,
+              keyboardType: TextInputType.multiline,
+            ),
+            IconButton(
+              icon: const Icon(Icons.post_add),
+              onPressed: () {
+                showDialog(context: context, builder: (context) {
+                  return AlertDialog(
+                    content: Text(textController.text),
+                  );
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
